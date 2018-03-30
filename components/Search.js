5cdf105a34ba1033931cf9c11397cd51384283de
ktgertig <35666978+ktgertig@users.noreply.github.com>
2018-03-30 13:06:18 +0100

    Update Search.js

--- components/Search.js
+++ components/Search.js
@@ -1,8 +1,6 @@
 Search = React.createClass({
     getInitialState() {
-        return {
-            searchingText: ''
-        };
+        return {searchingText: ''};
     },
 
     handleChange: function(event) {
@@ -11,21 +9,16 @@ Search = React.createClass({
         
         this.setState({searchingText: searchingText});
 
-        if (searchingText.length > 2) {
-            this.props.onSearch(searchingText);
-        }
+        if (searchingText.length > 2) {this.props.onSearch(searchingText);}
     },
 
     handleKeyUp: function(event) {
-        if (event.keyCode === 13) {
-            this.props.onSearch(this.state.searchingText);
-        }
+        if (event.keyCode === 13) {this.props.onSearch(this.state.searchingText);}
     },
 
     render: function() {
+        var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
-    var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
-
-    return <input type="text" onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder="Tutaj wpisz wyszukiwana fraze" style={styles} value={this.state.searchTerm}/>
+        return <input type="text" onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder="Tutaj wpisz wyszukiwana fraze" style={styles} value={this.state.searchTerm}/>
     }
 });
