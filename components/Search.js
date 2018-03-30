c4aba2851b673a91dd4dc53216e4dc3a53f060bb
ktgertig <35666978+ktgertig@users.noreply.github.com>
2018-03-30 13:10:27 +0100

    2nd update Search.js

--- components/Search.js
+++ components/Search.js
@@ -17,6 +17,7 @@ Search = React.createClass({
     },
 
     render: function() {
+        
         var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
         return <input type="text" onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder="Tutaj wpisz wyszukiwana fraze" style={styles} value={this.state.searchTerm}/>
