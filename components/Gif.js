6e288bb6ba7af23026bc579a8e3ab889ddf28775
ktgertig <35666978+ktgertig@users.noreply.github.com>
2018-03-30 13:07:14 +0100

    2nd Update Gif.js

--- components/Gif.js
+++ components/Gif.js
@@ -2,8 +2,8 @@ var GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
 var styles = {minHeight: '310px', margin: '0.5em'};
 
 Gif = React.createClass({
-  getUrl: function() {return this.props.sourceUrl || GIPHY_LOADING_URL;
-  },
+  getUrl: function() {return this.props.sourceUrl || GIPHY_LOADING_URL;},
+  
   render: function() {
     
     var url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
